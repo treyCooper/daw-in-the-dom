162c4835d1908c6f3fad63f07578dc90b71af65d
Trey <treycoopermusic@gmail.com>
2018-01-21 21:30:34 -0500

    add delay dial

--- scripts/app.js
+++ scripts/app.js
@@ -336,7 +336,7 @@ console.log('seq', sequencer)
 //const scale = ["C4", "B3", "A3", "G3", "F3",  "A3", "A#3", "B3", "C4"]
 var vol = new Tone.Volume(-30);
 const verb = new Tone.Freeverb(0.25).connect(Tone.Master);
-const seqDelay = new Tone.FeedbackDelay(0.2,0.2)
+const seqDelay = new Tone.FeedbackDelay(0.2,0.7)
 var filter = new Tone.Filter(5000, "lowpass");
 //let synthPreset = Tone.FMSynth
 
@@ -356,7 +356,11 @@ let volumeSlider = new Nexus.Slider("#volume", {
 controls.freq.value = 0.25
 controls.reverb.value = 0.25
 controls.delay.value = 0.5
-
+controls.delay.min = 0;
+controls.delay.max = 0.8;
+controls.delay.on('change',function(value) {
+	seqDelay.wet.value = value;
+})
 volumeSlider.colorize("fill","#d1d3d6")
 volumeSlider.on('change', function(v){
   volume.volume.cancelScheduledValues();
